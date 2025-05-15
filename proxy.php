2dfd03e1f3721b89706fc3b1e73786db45838653
Warorua <waroruaalex640@gmail.com>
2025-05-16 00:49:16 +0300

    ehrh

--- proxy.php
+++ proxy.php
@@ -34,7 +34,7 @@ if (json_last_error() !== JSON_ERROR_NONE || !isset($parsed['intercepted'][0]['f
         'error' => 'Full response is not valid JSON or expected format',
         'json_error' => json_last_error_msg(),
         'http_code' => $httpCode,
-        'raw_response' => $response
+        'raw_response' => $json
     ]);
     exit;
 }
